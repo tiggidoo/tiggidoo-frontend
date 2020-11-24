0a0961d5eea58a9526d687374e6415e95f11b570
Jonier Murillo <jonierm@gmail.com>
2020-11-24 15:51:16 -0500

    SEO has been updated on Become pro page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from 'react-helmet';
 import { Button, Container, Navbar, Nav } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
@@ -19,43 +20,54 @@ export default function Header() {
   if(lang === 'en'){
     langLabel = "Fr";
   }
+  console.log(t("SEO.meta.description"));
+
+
   return (
-    <Navbar bg="light" expand="xl" className="NavBar">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src={"images/logo_tiggidoo.svg"}
-            className="NavBar__logo"
-            alt=""
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <div>
+      <Helmet>
+        <title>{ t("SEO.title") }</title>
+        <meta name="description" content= {t("SEO.meta.description")}/>
+        <meta name="keywords" content= {t("SEO.meta.keywords")}  />
+      </Helmet>
 
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="mr-auto NavBar__menu">
-            <Nav.Link style={{fontSize: '20px'}} href="#home" className="NavBar__menu__item">
-              Services
-            </Nav.Link>
-            <Nav.Link style={{fontSize: '20px'}} href="/" className="NavBar__menu__item">
-              Devenir ToDoo
-            </Nav.Link>
-          </Nav>
+      <Navbar bg="light" expand="xl" className="NavBar">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={"images/logo_tiggidoo.svg"}
+              className="NavBar__logo"
+              alt=""
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-          <Button href="#home" size="lg" className="NavBar__btnLogin">
-            {t("Nav.menu.login")}
-          </Button>
-
-          <Nav className="NavBar__language">
-              <Nav.Link
-                href="#home"
-                className="NavBar__language__item"
-                onClick={() => changeLanguage(langLabel.toLowerCase())}
-              >
-                {langLabel}
+          <Navbar.Collapse className="justify-content-end">
+            <Nav className="mr-auto NavBar__menu">
+              <Nav.Link style={{fontSize: '20px'}} href="#home" className="NavBar__menu__item">
+                Services
               </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <Nav.Link style={{fontSize: '20px'}} href="/" className="NavBar__menu__item">
+                Devenir ToDoo
+              </Nav.Link>
+            </Nav>
+
+            <Button href="#home" size="lg" className="NavBar__btnLogin">
+              {t("Nav.menu.login")}
+            </Button>
+
+            <Nav className="NavBar__language">
+                <Nav.Link
+                  href="#home"
+                  className="NavBar__language__item"
+                  onClick={() => changeLanguage(langLabel.toLowerCase())}
+                >
+                  {langLabel}
+                </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
