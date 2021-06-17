463204c3ba1f8cf4dc87a9c43b65f8b9e5e62a47
amal chelbi <amal.chelbi@aleia.io>
2021-06-17 16:52:00 +0100

    add prestation sections responsive

--- src/components/views/client/sections/chooseService/Benefit.js
+++ src/components/views/client/sections/chooseService/Benefit.js
@@ -8,24 +8,22 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import DateFnsUtils from '@date-io/date-fns';
 import {
     MuiPickersUtilsProvider,
-    KeyboardTimePicker,
     KeyboardDatePicker,
 } from '@material-ui/pickers';
 
 const Benefit = ({ t }) => {
     const [value, setValue] = useState(0);
     const [state, setState] = useState({
-        dim: true,
-        lun: true,
-        mar: true,
-        mer: true,
+        dim: false,
+        lun: false,
+        mar: false,
+        mer: false,
         jeu: true,
-        vend: true,
+        ven: false,
         sam: true,
     });
 
@@ -33,10 +31,17 @@ const Benefit = ({ t }) => {
         setState({ ...state, [event.target.name]: event.target.checked });
     };
 
-    const [age, setAge] = useState('');
+    const [date, setTime] = useState({
+        day_one: '',
+        day_two: '',
+    });
 
     const handleChangeSelect = (event) => {
-        setAge(event.target.value);
+        const name = event.target.name;
+        setTime({
+            ...date,
+            [name]: event.target.value,
+        });
     };
 
     const [selectedDate, setSelectedDate] = useState(new Date('2021-08-18T21:11:54'));
@@ -44,6 +49,13 @@ const Benefit = ({ t }) => {
     const handleDateChangeDate = (date) => {
         setSelectedDate(date);
     };
+
+    const [isActive, setActive] = useState(false);
+
+    const handleClickOption = () => {
+        setActive(!isActive);
+        console.log("clicked")
+    }
 
     return (
         <Box className="HousingType">
@@ -53,7 +65,6 @@ const Benefit = ({ t }) => {
                 value={value}
                 onChange={(event, newValue) => {
                     setValue(newValue);
-                    console.log(value)
                 }}
 
                 showLabels
@@ -77,7 +88,7 @@ const Benefit = ({ t }) => {
 
                 <div className="options_list">
                     <OptionsCard hasCounter="true" />
-                    <OptionsCard title={t("Client.Benefit.section2_option2")} iconSrc="images/icon_spray.svg" description={t("Client.Benefit.section2_option2_desc")} hasCounter="true" />
+                    <OptionsCard title={t("Client.Benefit.section2_option2")} iconSrc="images/icon_spray.svg" description={t("Client.Benefit.section2_option2_desc")} hasCounter="true" onClick={handleClickOption}/>
                     <OptionsCard title={t("Client.Benefit.section2_option3")} iconSrc="images/icon_fridge.svg" description={t("Client.Benefit.section2_option3_desc")} hasCounter="true" />
                     <OptionsCard title={t("Client.Benefit.section2_option4")} iconSrc="images/icon_bed.svg" description={t("Client.Benefit.section2_option4_desc")} />
                     <OptionsCard title={t("Client.Benefit.section2_option5")} iconSrc="images/icon_eco_spray.svg" description={t("Client.Benefit.section2_option5_desc")} />
@@ -93,7 +104,7 @@ const Benefit = ({ t }) => {
                 <Box className="date_box__container">
                     <Box className="days__container">
                         <Box className="section__title">
-                            <svg class="MuiSvgIcon-root MuiStepIcon-root MuiStepIcon-active" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="12"></circle><text class="MuiStepIcon-text" x="12" y="16" text-anchor="middle">1</text></svg>
+                            <svg className="MuiSvgIcon-root MuiStepIcon-root MuiStepIcon-active" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="12"></circle><text className="MuiStepIcon-text" x="12" y="16" textAnchor="middle">1</text></svg>
                             <Typography variant="h6">{t("Client.Benefit.section3_option1")}</Typography>
                         </Box>
 
@@ -101,37 +112,37 @@ const Benefit = ({ t }) => {
                             <FormGroup>
                                 <FormControlLabel
                                     control={<Checkbox checked={state.dim} onChange={handleChange} name="dim" color="primary" />}
-                                    label="Dim"
+                                    label={t("Client.Days.dim")}
                                     labelPlacement="start"
                                 />
                                 <FormControlLabel
                                     control={<Checkbox checked={state.lun} onChange={handleChange} name="lun" color="primary" />}
-                                    label="Lun"
+                                    label={t("Client.Days.lun")}
                                     labelPlacement="start"
                                 />
                                 <FormControlLabel
                                     control={<Checkbox checked={state.mar} onChange={handleChange} name="mar" color="primary" />}
-                                    label="Mar"
+                                    label={t("Client.Days.mar")}
                                     labelPlacement="start"
                                 />
                                 <FormControlLabel
                                     control={<Checkbox checked={state.mer} onChange={handleChange} name="mer" color="primary" />}
-                                    label="Mer"
+                                    label={t("Client.Days.mer")}
                                     labelPlacement="start"
                                 />
                                 <FormControlLabel
                                     control={<Checkbox checked={state.jeu} onChange={handleChange} name="jeu" color="primary" />}
-                                    label="Jeu"
+                                    label={t("Client.Days.jeu")}
                                     labelPlacement="start"
                                 />
                                 <FormControlLabel
-                                    control={<Checkbox checked={state.vend} onChange={handleChange} name="vend" color="primary" />}
-                                    label="vend"
+                                    control={<Checkbox checked={state.ven} onChange={handleChange} name="ven" color="primary" />}
+                                    label={t("Client.Days.ven")}
                                     labelPlacement="start"
                                 />
                                 <FormControlLabel
                                     control={<Checkbox checked={state.sam} onChange={handleChange} name="sam" color="primary" />}
-                                    label="Sam"
+                                    label={t("Client.Days.sam")}
                                     labelPlacement="start"
                                 />
                             </FormGroup>
@@ -140,40 +151,41 @@ const Benefit = ({ t }) => {
 
                     <Box className="hours__container">
                         <Box className="section__title">
-                            <svg class="MuiSvgIcon-root MuiStepIcon-root MuiStepIcon-active" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="12"></circle><text class="MuiStepIcon-text" x="12" y="16" text-anchor="middle">2</text></svg>
+                            <svg className="MuiSvgIcon-root MuiStepIcon-root MuiStepIcon-active" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="12"></circle><text className="MuiStepIcon-text" x="12" y="16" textAnchor="middle">2</text></svg>
                             <Typography variant="h6">{t("Client.Benefit.section3_option2")}</Typography>
                         </Box>
 
-                        <FormControl>
-                            <InputLabel id="date_one">Age</InputLabel>
+                        <FormControl required>
+                            <InputLabel id="date_one">Les jeudis</InputLabel>
                             <Select
                                 labelId="date_one"
                                 id="date_one"
-                                value={age}
+                                value={date.date_one}
                                 onChange={handleChangeSelect}
                             >
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
+                                <option value={10}>{t("Client.Time.morning")}</option>
+                                <option value={20}>{t("Client.Time.afternoon")}</option>
+                                <option value={30}>{t("Client.Time.any")}</option>
                             </Select>
-
-                            <InputLabel id="date_two">Age</InputLabel>
+                            </FormControl>
+                            <FormControl required>
+                            <InputLabel id="date_two">Les samedis </InputLabel>
                             <Select
                                 labelId="date_two"
                                 id="date_two"
-                                value={age}
+                                value={date.date_two}
                                 onChange={handleChangeSelect}
                             >
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
+                                <option value={10}>{t("Client.Time.morning")}</option>
+                                <option value={20}>{t("Client.Time.afternoon")}</option>
+                                <option value={30}>{t("Client.Time.any")}</option>
                             </Select>
                         </FormControl>
                     </Box>
 
                     <Box className="firstdate__container">
                         <Box className="section__title">
-                            <svg class="MuiSvgIcon-root MuiStepIcon-root MuiStepIcon-active" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="12"></circle><text class="MuiStepIcon-text" x="12" y="16" text-anchor="middle">3</text></svg>
+                            <svg className="MuiSvgIcon-root MuiStepIcon-root MuiStepIcon-active" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><circle cx="12" cy="12" r="12"></circle><text className="MuiStepIcon-text" x="12" y="16" textAnchor="middle">3</text></svg>
                             <Typography variant="h6">{t("Client.Benefit.section3_option3")}</Typography>
                         </Box>
                         <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -183,7 +195,7 @@ const Benefit = ({ t }) => {
                                 format="MM/dd/yyyy"
                                 margin="normal"
                                 id="date-picker-inline"
-                                label="Date picker inline"
+                                label={t("Client.Time.a-partir-du")}
                                 value={selectedDate}
                                 onChange={handleDateChangeDate}
                                 KeyboardButtonProps={{
