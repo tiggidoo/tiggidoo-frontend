7ef440ba7d4d1e0619c38bcab4af3b7cb1e15640
amal chelbi <amal.chelbi@aleia.io>
2021-06-22 11:33:49 +0100

    localisation responsive

--- src/components/views/client/pages/Localisation.js
+++ src/components/views/client/pages/Localisation.js
@@ -10,10 +10,12 @@ import { Col, Row } from "react-bootstrap";
 import { Link } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
 import { Typography, Box } from '@material-ui/core'
-import InputPostCode from "../section-parts/InputPostCode";
-
+import usePostCode from '../functions/usePostCode'
+import validate from '../functions/validateInfo'
 
 function Localisation({ t }) {
+    const { handleChange, values, handleSubmit, errors } = usePostCode(validate)
+
     return (
         <div>
             <HeaderServ />
@@ -27,30 +29,46 @@ function Localisation({ t }) {
                         <p>{t("Client.Localisation.section1_text2")}</p>
                         <p>{t("Client.Localisation.section1_text3")}</p>
                     </Box>
-                    <InputPostCode />
-                    {/* <Button variant="contained" className="btn_green_bg">
-                        {t("Client.Localisation.section1_btn")}
-                    </Button> */}
+
+                    <Box className="post_code__form">
+                        <form action="" onSubmit={handleSubmit}>
+                            <Box className="post_code__input_container">
+                                <input id="postCode" type='text' name="postCode" placeholder={t("Client.Hero.postCodeLabel")} value={values.postCode} onChange={handleChange}></input>
+                            </Box>
+
+                            <input type='submit' value={t("Client.Localisation.section1_btn")} className="btn_green_bg" ></input>
+
+                            {errors.postCode && <p className='error'>{errors.postCode}</p>}
+                        </form>
+                    </Box>
                 </Col>
 
                 <Col md={2} className="Localisation_seperator__container">
                     <Box>
-                    <span></span>
-                    <p className="Localisation_seperator">
-                        {t("Client.Localisation.ou")}
-                    </p>
+                        <span></span>
+                        <p className="Localisation_seperator">
+                            {t("Client.Localisation.ou")}
+                        </p>
                     </Box>
                 </Col>
 
-                <Col md={5}>
+                <Col md={5} className="">
                     <Typography variant="h3" className="">{t("Client.Localisation.section2_title")}</Typography>
 
                     <p>{t("Client.Localisation.section2_text1")}</p>
+
+
+                    <Box className="Localisation_subtext">
+                        <p> <a href="#">{t("Client.Localisation.section2_text2")}</a></p>
+                    </Box>
 
                     <Button className="btn_green_bg">
                         {t("Client.Localisation.section2_btn")}
                     </Button>
                 </Col>
+            </Row>
+            <Row className="localisation_footer">
+                <p>{t("Client.Localisation.footer")} <a href="#" className="link">{t("Client.Localisation.footer_link")} </a></p>
             </Row>
             <Footer />
         </div>
