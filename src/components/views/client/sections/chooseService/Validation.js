49a84d4e7dff57abe36860185fb0486bf1425376
Vincent Veysset <vincent.veysset.canada@gmail.com>
2021-07-16 15:42:54 +0200

    Update phone input field

--- src/components/views/client/sections/chooseService/Validation.js
+++ src/components/views/client/sections/chooseService/Validation.js
@@ -52,11 +52,14 @@ const Validation = ({ t }) => {
     }
 
     const handlePersonalDataChange = (event) => {
-        const name = event.target.name;
+        let name = event.target.name;
         let value = event.target.value;
 
         if (name === 'email') value = value.trim().replace(/\s/g, '');
-        if (name === 'telephone') value = value.trim().replace(/\D/g, '');
+        if (name === '') {
+            name = 'telephone';
+            value = `+${value.trim().replace(/\D/g, '')}`;
+        }
 
         setPersonalData({ ...personalData, [name]: value });
     };
@@ -85,7 +88,7 @@ const Validation = ({ t }) => {
         if (email === '') errors.email = true;
         if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) errors.email = true;
         if (telephone === '') errors.telephone = true;
-        if (telephone.length !== 10) errors.telephone = true;
+        if (telephone.length < 11) errors.telephone = true;
         if (password === '') errors.password = true;
         if (confirmPassword === '') errors.confirmPassword = true;
         if (password !== confirmPassword) errors.password = errors.confirmPassword = true;
@@ -102,7 +105,6 @@ const Validation = ({ t }) => {
         if (Object.keys(dataValidation()).length !== 0) return;
 
         const requestBody = { ...store.getState().estimation.settings, ...personalData, lag: 'fr' };
-        requestBody.telephone = `+1${requestBody.telephone}`;
 
         estimationPersonalDataUpdate(requestBody)(dispatch);
 
@@ -323,14 +325,18 @@ const Validation = ({ t }) => {
                                 variant="outlined"
                                 error={errors?.email ? true : false}
                             />
+
                             <InputCustomPhone
                                 name="telephone"
-                                value={personalData.telephone}
+                                country="ca"
                                 onChange={handlePersonalDataChange}
-                                type="phone"
-                                label={t("Client.Validation.phone")}
+                                value={personalData.telephone}      
+                                disableCountryCode={false}
+                                disableDropdown={false}
+                                enableSearch={true}
+                                regions={['north-america', 'europe']}
                                 error={errors?.telephone ? true : false}
-                            ></InputCustomPhone>
+                            />
 
                             <FormControl variant="outlined">
                                 <InputLabel htmlFor="standard-adornment-password">{t("Client.Validation.password")}</InputLabel>
